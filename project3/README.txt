a7f4fd0b6fe16564679ad10e07b4e26e87f40769
Hageli <miro.hageli@.com>
2024-01-20 20:01:52 +0200

    final commit project 3

---
+++ project3/README.txt
@@ -0,0 +1,5 @@
+This folder contains project 3
+
+Wish.c is the file that contains the shell. Input.txt can be used to test the batch mode in out shell
+
+-M.H.
--- project3/wish.c
+++ project3/wish.c
@@ -1,73 +1,199 @@
+/*
+TEKIJÄ: MIRO HAGELBERG
+OPISKELIJANUMERO: 0604373
+VIIMEKSI MUOKATTU: 19.1.2024
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
+// Declaring functions used in this file
+int execute(char *path, char **command);
+char **parseCommand(char* line);
+int commandSize(char **commands);
+void exec_cd(char *command, int temp);
 
-
-
-int execute(char** command) {
-    int rc_wait;
-    int rc = fork();
-    char* PATH = (char*)malloc(sizeof(char*) * 5 + sizeof(char*) * strlen(command[0]));
-    strcpy(PATH, "/bin/");
+// This executes non-built in functions
+int execute(char *path, char **command) {
+    // Creating the path 
+    char *PATH = malloc(256);
+    strcpy(PATH, path);
     strcat(PATH, command[0]);
-    if (rc < 0) {
-        fprintf(stderr, "Fork failed.\n");
-    } else if (rc == 0) {
-        if (access(PATH, X_OK) == 0) {
+    // Creating the subprocess
+    int subp = fork();
+    // If fork fails
+    if(subp < 0) {
+        fprintf(stderr, "Fork failed\n");
+    // Trying to execute command if path is accessible
+    } else if(subp == 0) {
+        if(access(PATH, X_OK) == 0) {
             execv(PATH, command);
+        // Returning 0 if unable to
         } else {
-            fprintf(stderr, "Unknown command.");
+            free(PATH);
+            return 0;
         }
     } else {
-        rc_wait = wait(NULL);
+        wait(NULL);
     }
-    free(PATH); 
+    free(PATH);
     return 1;
 }
 
-int parseCommand(char* line) {
-    int i = 0, status = 1;
+// This gets us the command tokens
+char **parseCommand(char* line) {
+    int i = 0;
     char* token = NULL;
     char* delim = " ";
-    char** command = (char**)malloc(strlen(line) * sizeof(char*));
+    char** command = malloc(100 *sizeof(char*));
     while ((token = strtok_r(line, delim, &line)) != NULL) {
         command[i] = strdup(token);
         i++;
     }
     command[i + 1] = NULL;
-    status = execute(command);
-    for (int j = 0; j < i + 1; j++) {
-        free(command[j]);
-    }
-    free(command);
-    if (status == 0)
-        return 0;
-    return 1;
+    return command;
 }
 
-void listen() {
-    int status = 1;
-    size_t length = 0;
-    char* line = NULL;
-    printf("wish> ");
-    while (getline(&line, &length, stdin) != -1) {
-        int temp = strlen(line);
-        if(temp != 1) {
-            line[strlen(line) - 1] = '\0';
-            status = parseCommand(line);
-            if (status == 0)
-                break;
-            }
-        printf("wish> ");
+// This is used to get the amount of command args
+int commandSize(char **commands) {
+    int command_size = 0;
+    while(commands[command_size] != NULL) {
+        command_size++;
     }
-    free(line);
-    return;
+    return command_size;
 }
+
+// This executes built-in cd
+void exec_cd(char *command, int temp) {
+    if(temp == 2) {
+        if(chdir(command) != 0) {
+            fprintf(stderr, "Chdir failed\n");
+        }
+    } else {
+        fprintf(stderr, "No destination folder\n");
+    }
+}
+
 
 int main(int argc, char** argv) {
-    listen();
-    return 0;
+    char *line = NULL;
+    size_t length = 0;
+    char *path[100] = {"/bin/"};
+    char **commands;
+    int temp = 0;
+    int status = 0;
+    int path_status = 0;
+    // Interactive mode
+    if(argc == 1) {
+        printf("wish> ");
+        while(getline(&line, &length, stdin) != -1) {
+            if(strlen(line) > 1) {
+                line[strlen(line) - 1] = '\0';
+                commands = parseCommand(line);
+                temp = commandSize(commands);
+                // built-in exit
+                if(strcmp(commands[0], "exit") == 0) {
+                    if(temp > 1) {
+                        fprintf(stderr, "Exit does not accept arguments\n");
+                    } else {
+                        free(line);
+                        free(commands);
+                        exit(0);
+                    }
+                // Built-in cd
+                } else if(strcmp(commands[0], "cd") == 0) {
+                    exec_cd(commands[1], temp);
+                // Built-in path
+                } else if(strcmp(commands[0], "path") == 0) {
+                    // if path is empty, change status
+                    if(temp == 1) {
+                        path_status = 1;
+                    } else {
+                        // Setting new paths
+                        for(int i = 0; i < temp - 1; i++) {
+                            path[i] = commands[i + 1];
+                        }
+                        path[temp] = NULL;
+                    }
+                } else {
+                    // if path is empty, do nothing
+                    if(path_status == 0) {
+                        int counter = 0;
+                        while(path[counter] != NULL) {
+                            status = execute(path[counter], commands);
+                            // break if execute is successful
+                            if(status == 1) {
+                                break;
+                            }
+                            counter++;
+                        }
+                    }
+                }
+            }
+            printf("wish> ");
+        }
+    // Batch mode
+    } else if (argc == 2) {
+        for(int j = 1; j < argc; j++) {
+            // Exit if file open fails
+            FILE *file = fopen(argv[j], "r");
+            if(file == NULL) {
+                fprintf(stderr, "wish: cannot open file\n");
+                exit(1);
+            }
+            while(getline(&line, &length, file) != -1) {
+                if(strlen(line) > 1) {
+                line[strlen(line) - 1] = '\0';
+                commands = parseCommand(line);
+                temp = commandSize(commands);
+                // built-in exit
+                if(strcmp(commands[0], "exit") == 0) {
+                    if(temp > 1) {
+                        fprintf(stderr, "Exit does not accept arguments\n");
+                    } else {
+                        free(line);
+                        free(commands);
+                        exit(0);
+                    }
+                // Built-in cd
+                } else if(strcmp(commands[0], "cd") == 0) {
+                    exec_cd(commands[1], temp);
+                // Built-in path
+                } else if(strcmp(commands[0], "path") == 0) {
+                    // if path is empty, change status
+                    if(temp == 1) {
+                        path_status = 1;
+                    } else {
+                        // Setting new paths
+                        for(int i = 0; i < temp - 1; i++) {
+                            path[i] = commands[i + 1];
+                        }
+                        path[temp] = NULL;
+                    }
+                } else {
+                    // if path is empty, do nothing
+                    if(path_status == 0) {
+                        int counter = 0;
+                        while(path[counter] != NULL) {
+                            status = execute(path[counter], commands);
+                            // break if execute is successful
+                            if(status == 1) {
+                                break;
+                            }
+                            counter++;
+                        }
+                    }
+                }
+            }
+            }
+            fclose(file);
+        }
+    // Too many arguments
+    } else {
+        exit(1);
+    }
+
 }
